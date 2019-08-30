a007c79d6d8731b9062adc9641448474a6b7233f
Prem <premsahni0501@gmail.com>
2019-08-30 12:55:09 +0530

    Updates

---
+++ precache-manifest.19acba23cdd017051bed8357f827f4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0563e8dbac1d1b1914e7cb9d0379f06",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "7a0646b832823b5f6276",
+    "url": "/react-redux-todo/static/css/main.adba6ba9.chunk.css"
+  },
+  {
+    "revision": "0652f7a6c4d111770d08",
+    "url": "/react-redux-todo/static/js/2.59f57cf5.chunk.js"
+  },
+  {
+    "revision": "7a0646b832823b5f6276",
+    "url": "/react-redux-todo/static/js/main.2983bc8c.chunk.js"
+  },
+  {
+    "revision": "82d88a4e62635755860e",
+    "url": "/react-redux-todo/static/js/runtime~main.0255c499.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
